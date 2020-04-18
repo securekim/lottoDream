8186cf4406b3af068f1419acb1e3c7c71524400d
securekim <brodotkim@gmail.com>
2020-04-19 04:11:23 +0900

    Add google ads and change script for start

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 echo "LIST OF SERVER PROCESSES" 
-pm2 list
-pm2 kill
-pm2 start index.js --name=nodeServer
-pm2 start dreamAnalyzer/analyzer_dream_wikidocs.py --name=pythonServer --interpreter /usr/bin/python3
+/usr/bin/pm2 list
+#pm2 kill
+/usr/bin/pm2 stop nodeServer
+/usr/bin/pm2 stop pythonServer
+/usr/bin/pm2 start ~/lottoDream/index.js --name=nodeServer
+/usr/bin/pm2 start ~/lottoDream/dreamAnalyzer/analyzer_dream_wikidocs.py --name=pythonServer --interpreter /usr/bin/python3
 
