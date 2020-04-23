4f71484d9261d4ba971e2ad409b73f307f5e7e31
securekim <brodotkim@gmail.com>
2020-04-23 16:46:59 +0900

    [Feature] Change start script

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo "LIST OF SERVER PROCESSES"
 /usr/bin/pm2 stop pythonServer
 /usr/bin/pm2 start ~/lottoDream/index.js --name=nodeServer
 /usr/bin/pm2 start ~/lottoDream/dreamAnalyzer/analyzer_dream_wikidocs.py --name=pythonServer --interpreter /usr/bin/python3
-
+echo "Logs will be saved in /home/makers/.pm2/logs"
